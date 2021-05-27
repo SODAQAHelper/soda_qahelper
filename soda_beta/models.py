86b6bffc6ac5c1e8551b792435f4ec01add2aa97
liye <lyyoo38@gmail.com>
2021-05-28 06:49:03 +0900

    [수정] 임시버전

--- soda_beta/models.py
+++ soda_beta/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 class Information(models.Model):
     name = models.CharField(max_length=20, null=True, blank=True)
@@ -8,17 +7,14 @@ class Information(models.Model):
     class Meta:
         db_table = 'global_information'
 
-
 class Makeup(models.Model):
     builtin_id = models.IntegerField(null=True, blank=True)
     type = models.CharField(max_length=10, null=False, blank=False)
     makeup_name = models.CharField(max_length=255, null=False, blank=False)
     name = models.CharField(max_length=255, null=False, blank=False)
     sub_name = models.TextField(null=True, blank=True)
-
     class Meta:
         db_table = 'makeup'
-
 
 """
 builtin_id           int          null,
--- soda_beta/views.py
+++ soda_beta/views.py
@@ -20,16 +20,13 @@ class MainView(TemplateView):
 
 class StyleView(TemplateView):
     template_name = 'soda_beta/helper_style.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         return context
-
     def get(self, request, app, *args, **kwargs):
         context = self.get_context_data()
         context['app'] = app
         return self.render_to_response(context=context)
-
 
 class QAHelperView(TemplateView):
     template_name = 'soda_beta/soda_qahelper.html'
@@ -48,7 +45,6 @@ class QAHelperView(TemplateView):
         # context['model'] = obj
         # Information.objects.raw('SELECT "name" from ')
         return context
-
     def get(self, request, type, *args, **kwargs):
         context = self.get_context_data()
         # res = requests.get('http://qa-content-b612-api.snow.me/v1/text/overview')
@@ -59,13 +55,12 @@ class QAHelperView(TemplateView):
     # 저장할때 보통 POST쓰고, page를 보통 load 하는것이 get
     # 보통 위와 같이 쓴다. 중간 과정에서 데이터를 주고받음
 
-
 def qahelper(request):
     if request.method == "POST":
         data = request.POST.get('gender')
         # 데이터를 얻었다 -> 거기에 저장할 때 이름으로 데이터를 저장해서 전달한다. 프론트엔드에서 정하고 백엔드에서 알고 있어야 한다.
         return render(request)
     # 쌩으로 동작 시켜주는 것이 request, 그래서 페이지가 확실히 구분되어 불러올 때 함수형쓰고, 페이지 내부에서 ajax를 쓰거나 한다.
-    # render 쫓아가 보면 다 구조화 시켜줬다.
+    #render 쫓아가 보면 다 구조화 시켜줬다.
 
 #
